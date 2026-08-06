04f2ac45fe77f19471b8071427a97ee83a441268
Afreed7411 <135531217+Afreed7411@users.noreply.github.com>
2026-08-06 14:49:36 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
           letterSpacing: "1.2px"
         }}
       >
-        Welcome To Ethnotech and my website
+        Welcome To Ethnotech and my website   and welcome to SIT
       </h1> 
     </div>
   );
